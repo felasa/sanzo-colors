575c833ff5360aba5c85448b4bae1349439cd902
felasa <felasa@gmail.com>
2025-06-29 21:24:37 -0600

    add hsb

--- src/process_colors.clj
+++ src/process_colors.clj
@@ -54,17 +54,32 @@
         o-cieXYZ (. o-space toCIEXYZ cmyk-arr)]
     (vec (. d-space fromCIEXYZ o-cieXYZ)))) 
 
+(defn rgb->hsb 
+  "RGB base 255 to HSB"
+  [r g b]
+  (let [hsb_b1 (. java.awt.Color RGBtoHSB r g b nil)]
+    (->> hsb_b1 (mapv * [359 100 100]) (mapv math/round))))
+
 (comment
   (cmyk->rgb    0.2 0.4 0.9 0.15)  ;; [0.70666057 0.5323262 0.2054322]
-  (rc-cmyk->rgb 0.2 0.4 0.9 0.15)) ;; [0.71459526 0.5489586 0.2640421]
+  (rc-cmyk->rgb 0.2 0.4 0.9 0.15) ;; [0.71459526 0.5489586 0.2640421]
+  (rgb->hsb 255 20 60) ;; [239 67 24]
+  (java.awt.Color. 1 0 0)
+  (let [cs (new java.awt.color.ICC_ColorSpace 
+                (. java.awt.color.ICC_Profile getInstance 
+                   "resources/profiles/sRGB_IEC61966-2.1.icc"))]
+    (apply (java.awt.Color) (. cs toRGB (float-array [1 0 0]))))
+  (. java.awt.Color RGBtoHSB 255 0 0 nil))
 
 (defn add-rgb
   "Adds rgb info to a map"
   [kv]
   (let [cmyk (map #(/ % 100) (kv "cmyk"))
-        rgb (map #(math/round (* 255 %))  (apply cmyk->rgb cmyk))
-        hex (apply rgb->hex rgb)]
-    (assoc kv "rgb" (vec rgb) "hex" hex)))
+        rgb (map #(math/round (* 255 %)) (apply cmyk->rgb cmyk))
+        hex (apply rgb->hex rgb)
+        hsb (apply rgb->hsb rgb)]
+    (-> (assoc kv "rgb" (vec rgb) "hex" hex)
+        (assoc "hsb" hsb))))
 
 (def colors_l0
   (->> ["swatches_a" "swatches_b" "swatches_c"
@@ -80,7 +95,8 @@
   (let [colors (filter (fn [c] (contains? (set (c "combinations")) combo-id)) colors_l0)]
     {"id_combo" combo-id
      "id_colors" (mapv #(% "id_color") colors)
-     "hex" (mapv #(% "hex") colors)}))
+     "hex" (mapv #(% "hex") colors)
+     "hsb" (mapv #(% "hsb") colors)}))
      
 (def combos (for [combo_id (range 1 349)]
               (get-combo-info combo_id)))
@@ -93,7 +109,7 @@
         (json/write-str data {:indent true :escape-slash false})))
 
 (comment
-  (write! "sanzo-colors.json"))
+  (write! "sanzo-colors_hsb.json"))
 
 (defn -main
   "Invoke me with clojure -M -m process-colors filename"
